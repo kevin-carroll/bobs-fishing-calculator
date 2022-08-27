1ebbf7553e01f9bfb66465db74532f93e24f5dfd
Kevin Carroll <kevin.carroll@outlook.com>
2022-08-27 11:31:50 -0700

    added pr script (#1)
    Initial PR script is set

---
+++ build/configure-ci-build.ps1
@@ -0,0 +1,23 @@
+
+# Copy the strict linting file in place of the regular one
+# this will enforce build errors on any applicable warnings during development
+$strictLintPath = "./build/strict-linting.ruleset"
+$developerLintPath = "./src/styles.ruleset"
+
+Write-Host "Configuring Environment for CI Build" -ForegroundColor Yellow
+
+if(-Not (Test-path $developerLintPath)){
+    Write-Host "Invalid Path. Unable to locate $developerLintPath" -ForegroundColor Red
+    return 1
+}
+
+if(-Not (Test-path $strictLintPath)){
+    Write-Host "Invalid Path. Unable to locate $strictLintPath"  -ForegroundColor Red
+    return 1
+}
+
+Write-Host "Enforcing Strict Linting (Stylecop)..."
+Remove-Item $developerLintPath
+Copy-Item  $strictLintPath -Destination $developerLintPath
+
+Write-Host "CI Build Configuration Complete" -ForegroundColor Green
