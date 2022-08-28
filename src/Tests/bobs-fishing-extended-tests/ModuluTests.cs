053ed81a13f8d836729ebc780a5f990be1084f70
bobs-fishing-library <112338782+bobs-fishing-library@users.noreply.github.com>
2022-08-28 09:53:03 -0700

    Added Modulo Function to Extended Library (#13)
    * added modulo
    
    * added another test
    
    Co-authored-by: Kevin Carroll <kevin.carroll@outlook.com>
    Co-authored-by: Bobs FIshing <bob.fishing.library@gmail.com>

---
+++ src/Tests/bobs-fishing-extended-tests/ModuluTests.cs
@@ -0,0 +1,22 @@
+// *************************************************************
+// project:  Bobs Fishing Calculator!!
+// *************************************************************
+
+namespace BobsFishing.Calculator.Extended.Tests
+{
+    using BobsFishing.Calculator.Extended;
+    using NUnit.Framework;
+
+    public class ModuluTests
+    {
+        [TestCase(4, 2, 0)]
+        [TestCase(10, 6, 4)]
+        [TestCase(20, 19, 1)]
+        public void Modulu(int a, int b, int expectedResult)
+        {
+            var entity = new BobsExtendedFishingCalculator();
+
+            Assert.AreEqual(expectedResult, entity.Modulo(a, b));
+        }
+    }
+}
